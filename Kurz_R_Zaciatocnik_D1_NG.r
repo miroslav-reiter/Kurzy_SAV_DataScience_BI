a465cd91359c7b443c796371bc561af9815d8875
Miroslav Reiter <riaditel@it-academy.sk>
2023-05-10 01:40:39 +0200

    Add files via upload

---
+++ Kurz_R_Zaciatocnik_D1_NG.r
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+
+# -- D1 --
+
+# # 📊 Kurz Jazyk R I. Začiatočník
+# **🧑‍🏫 Lektor:** Miroslav Reiter  
+# **📥 LinkedIn kontakt:** https://www.linkedin.com/in/miroslav-reiter/  
+# 
+# **✅ Osnova:** https://itkurzy.sav.sk/node/161
+# 
+# **🎞️ YouTube videá:** https://www.youtube.com/c/IT-AcademySK  
+# **📇 Zdrojové kódy a materiály:** https://github.com/miroslav-reiter/Kurzy_SAV_Analytika_Python_R  
+# 
+# **😊 Emojis:** Win + .  
+# **🔖 MarkDown Dokumentácia:** https://www.markdownguide.org/getting-started/
+
+
+# # 🆘 Markdown a R-Markdown
+# Jednoduchý **značkovací jazyk**, ktorý slúži na **úpravu jednoduchého textu** a jeho následný **prevod na formátovaný text** publikovateľný na webe, najmä vo **formáte HTML**. Umožňuje pomocou jednoduchých formátovacích značiek vyznačiť v texte nadpisy a zoznamy, doplniť odkazy, obrázky atď. 
+# 
+# Cieľom jazyka je, aby bol **text dobre čitateľný** a **publikovateľný** aj v pôvodnom formáte jednoduchého textu.
+# 
+# ```markdown
+# # (Znak Mriežky/Hashes) - Nadpisy (Heading) - Ctrl + [
+# ```
+# # (Znak Mriežky/Hashes) - Nadpisy (Heading) 
+# 
+# ```markdown
+# # Hlavný Nadpis - Nadpis 1. úrovne H1
+# ```
+# # Hlavný Nadpis - Nadpis 1. úrovne H1
+# ```markdown
+# 
+# ## Podnadpis - Nadpis 2. úrovne H2
+# ```
+# ## Podnadpis - Nadpis 2. úrovne H2
+# ```markdown
+# 
+# ## Podpodnadpis - Nadpis 3. úrovne H3
+# ```
+# ## Podpodnadpis - Nadpis 3. úrovne H3
+# 
+# ```markdown
+# **Tučný Text** (Znak 2x Hiezdička/Asterisk) - Tučný Text (Bold) - Ctrl + B
+# ```
+# **Tučný Text** (Znak 2x Hiezdička/Asterisk) - Tučný Text (Bold)
+# 
+# ```markdown
+# *Kurzíva Text* (Znak 1x Hiezdička/Asterisk) - Kurzíva Text (Italics) - Ctrl + I
+# ```
+# *Kurzíva Text* (Znak 1x Hiezdička/Asterisk) - Kurzíva Text (Italics)  
+# 
+# ```markdown
+# ~~Prečiarknutý Text~~ (Znak 2x Vlnovka/Tilde) - Prečiarknutý Text (Strike) - Alt + S
+# ```
+# ~~Prečiarknutý Text~~ (Znak 2x Vlnovka/Tilde) - Prečiarknutý Text (Strike) - Alt + S
+# 
+# 
+# ```markdown
+# - Priemer
+# - Medián
+# - Modus
+# ```
+# Nečíslovaný Zoznam (Unordered list UL) - Ctrl + l
+# - Priemer
+# - Medián
+# - Modus
+# 
+# ```markdown
+# - Priemer
+# - Medián
+# - Modus
+# ```
+# Číslovaný Zoznam (Ordered list OL) 
+# 1. Aritmetický Priemer
+# 2. Harmonický Priemer
+# 3. Geometrický Priemer
+# 
+# ```{r echo=T}
+# 5+6
+# 5*6/7*8
+# (5*6)/7*8
+# ```
+
+
+# # Príklad Jazyk Markdown 
+# 
+
+
+# # 🎁 Akú verziu Jazyka R používame a kde je Rko nainštalované?
+# Funkcie home a version
+
+
+# Kde je Rko nainštalované?
+
+
+# Komplexné informácie o verzií, platforme, os, revízie a iné
+
+
+# Vytiahne informáciu o os a platforme
+# $ Pomenovaný zoznam (list), dátový rámec (dataframe), podmnožina stĺpcov (subset)
+
+
+# Alternatíva cez funkcie
+
+# # 🖨 Používateľský Výstup, Tlač
+
+
+# Mriežka (Alt + 35) - Komentár (Poznámky a Dokumentácia)
+
+
+# Využitie Premenných (Variables)
+
+
+
+# Príkazový Riadok (CMD)
+# Rscript ahojsvet.R
+
+# # 🎲 Aritmetické Operátory (Operácie) pre Skalárne Dátové Typy
+
+
+# + - * / ;
+
+
+# Mocnina
+
+
+# Realne delenie
+
+
+# Delenie so zvyskom (modulo)
+
+
+# Celociselne delenie
+
+
+# Infix Operátory Backticks ` `
+# Môžete zavolať operátor ako funkciu
+
+# # 🔥 Zabudované Funkcie a Konštanty
+
+
+# Zaokruhlovanie, orezanie
+
+
+# Aritmetika
+
+
+# Plot Grafy
+
+
+# Character
+
+
+
+
+# # 💡 Zabudované Konštanty
+
+
+# Konštanty
+
+# # 🗳️ Premenné/Objekty a Dátové Typy
+
+
+# 5 skalárnych dátových typov sú číselné (numeric), znakové (character), celočíselné (integer), logické (logical) a komplexné (complex)
+# Názov premennej priradenie hodnoty (= alebo <-)
+
+
+# # ❇️ Formátovanie, Typy, Triedy
+
+
+# Ukoncovacie sekvencie, escaped sequences
+# \n new line novy riadok/odriadkuj
+# \t tab tab
+# \\ \
+# \" "
+# \' '
+
+# ## 📊 Generovanie Dát a Grafy
+
+
+
+
+# Data
+
+# Inkluzivne
+
+
+# # ✅ Zabudované Štatistické Funkcie
+
+
+
+
+# dnorm
+
+
+# pnorm
+
+# rnorm
+
+# dbinom
+
+
+# # 🏢 Workspace
+
+
+
+
+
+
+
+
+
+
+
+# # ℹ️ Bonus (packages)
+
+
+
+
